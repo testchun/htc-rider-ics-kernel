d4e54d35c7e3d5394a05698031c998b3ca35cb91
Jian Li <pyguni@gmail.com>
2012-08-12 00:09:43 +0900

    [CPIL] Add missing files for completing compilation

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Sat Aug 11 23:53:58 KST 2012"
+#define LINUX_COMPILE_BY "guni"
+#define LINUX_COMPILE_HOST "austin"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
