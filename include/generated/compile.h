22b39025520dc1814a2980f4161d642c8c02ec0e
Jian Li <pyguni@gmail.com>
2012-08-12 00:21:14 +0900

    [USB] Fast AC charging for Unknown device type

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 1 */
+/* This file is auto generated, version 2 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Sat Aug 11 23:53:58 KST 2012"
+#define UTS_VERSION "#2 SMP PREEMPT Sun Aug 12 00:15:00 KST 2012"
 #define LINUX_COMPILE_BY "guni"
 #define LINUX_COMPILE_HOST "austin"
 #define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
