e276cf23398e08c8ced4a4233f875e24bdadb6aa
Jian Li <pyguni@gmail.com>
2012-08-12 17:37:26 +0900

    [CPU] Implemented kernel based mpdecision
    CREDITED by oliverclaude

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 4 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#4 SMP PREEMPT Sun Aug 12 16:35:00 KST 2012"
-#define LINUX_COMPILE_BY "guni"
-#define LINUX_COMPILE_HOST "austin"
-#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
