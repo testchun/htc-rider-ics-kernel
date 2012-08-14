a2cafa776a1cff9c3b8d1f56e4d65cbb4fd29165
Jian Li <pyguni@gmail.com>
2012-08-14 09:09:20 +0900

    Redo "[USB] Add USB On-The-Go (OTG) capability"
    This reverts commit 1481c9427208a92527179b53810cb562b8464200.

--- arch/arm/mach-msm/devices-msm8x60.c
+++ arch/arm/mach-msm/devices-msm8x60.c
@@ -1918,7 +1918,7 @@ struct platform_device msm_device_gadget_peripheral = {
 		.coherent_dma_mask	= 0xffffffffULL,
 	},
 };
-#ifdef CONFIG_USB_EHCI_MSM_72K
+
 static struct resource resources_hsusb_host[] = {
 	{
 		.start	= 0x12500000,
@@ -1943,6 +1943,7 @@ struct platform_device msm_device_hsusb_host = {
 	},
 };
 
+#ifdef CONFIG_USB_EHCI_MSM_72K
 static struct platform_device *msm_host_devices[] = {
 	&msm_device_hsusb_host,
 };
