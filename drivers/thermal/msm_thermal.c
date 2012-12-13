a75857de5358289d215d661eab09405612a76b28
Gyu hyun, Park <baljern0603@gmail.com>
2012-12-13 20:40:49 +0900

    [THERMAL] Adjust thermal limit

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -23,16 +23,16 @@
 #define DEF_TEMP_SENSOR      0
 
 //max thermal limit
-#define DEF_ALLOWED_MAX_HIGH 76
-#define DEF_ALLOWED_MAX_FREQ 384000
+#define DEF_ALLOWED_MAX_HIGH 75
+#define DEF_ALLOWED_MAX_FREQ 486000
 
 //mid thermal limit
-#define DEF_ALLOWED_MID_HIGH 72
-#define DEF_ALLOWED_MID_FREQ 648000
+#define DEF_ALLOWED_MID_HIGH 69
+#define DEF_ALLOWED_MID_FREQ 864000
 
 //low thermal limit
-#define DEF_ALLOWED_LOW_HIGH 70
-#define DEF_ALLOWED_LOW_FREQ 972000
+#define DEF_ALLOWED_LOW_HIGH 63
+#define DEF_ALLOWED_LOW_FREQ 1188000
 
 //Sampling interval
 #define DEF_THERMAL_CHECK_MS 1000
